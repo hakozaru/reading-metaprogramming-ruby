be20fc9bf3ca8cde894f083baa8843a41573bea6
hakozaru <t.morii@everyleaf.com>
2021-06-24 09:50:16 +0900

    try 03_define

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -2,6 +2,12 @@
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
 
+class A1
+  define_method('//') do
+    '//'
+  end
+end
+
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -12,7 +18,38 @@
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
 
+class A2
+  def initialize(arr)
+    arr.each do |val|
+      define_singleton_method("hoge_#{val}") do |count|
+        return dev_team if count.nil?
+        "hoge_#{val}" * count
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
+
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+module OriginalAccessor
+  def self.included(klass)
+    klass.define_singleton_method(:my_attr_accessor) do |name|
+      attr_reader name
+
+      define_method("#{name}=") do |val|
+        instance_variable_set("@#{name}", val)
+
+        return unless [TrueClass, FalseClass].any? { |bool| public_send(name).is_a?(bool) }
+
+        define_singleton_method("#{name}?") { public_send(name) }
+      end
+    end
+  end
+end
