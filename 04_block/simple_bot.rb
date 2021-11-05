eaa52d8a3de2258be4143dba835a9531d746c23d
hakozaru <dragontails1025@gmail.com>
2021-11-05 12:16:50 +0900

    try 04_simple_bot

--- 04_block/simple_bot.rb
+++ 04_block/simple_bot.rb
@@ -22,3 +22,25 @@
 # 4. クラスメソッドsettingは、引数を2つ取り、1つ目がキー名、2つ目が設定する値です
 #     1. settingメソッドに渡された値は、クラスメソッド `settings` から返されるオブジェクトに、メソッド名としてアクセスすることで取り出すことができます
 #     2. e.g. クラス内で `setting :name, 'bot'` と実行した場合は、respondメソッドに渡されるブロックのスコープ内で `settings.name` の戻り値は `bot` の文字列になります
+
+class SimpleBot
+  @@keyword_block_mapped ||= {}
+
+  def self.respond(keyword, &block)
+    @@keyword_block_mapped[keyword] = block
+
+    define_method(:ask) do |obj|
+      return unless @@keyword_block_mapped.keys.include?(obj)
+      @@keyword_block_mapped[obj].call
+    end
+  end
+
+  def self.setting(key, value)
+    @settings_klass ||= Class.new
+    @settings_klass.define_singleton_method key, ->{ value }
+
+    define_singleton_method(:settings) do
+      @settings_klass
+    end unless respond_to?(:settings)
+  end
+end
